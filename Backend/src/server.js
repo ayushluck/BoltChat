ee619a7b65072623934547102f0c23b9f6c85142
ayushluck <luckyayush2005@gmail.com>
2026-05-19 15:55:13 +0530

    SIgnup controller added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,19 +4,23 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import connectDB from './lib/db.js';
 
 dotenv.config();
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const Port = process.env.PORT || 3000;
+const frontendDistPath = path.join(__dirname, '../../Frontend/dist');
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.use(express.static(frontendDistPath));
     app.get('*', (_,res)=>{
-        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+        res.sendFile(path.join(frontendDistPath, 'index.html'));
     });
 }
 
@@ -25,4 +29,5 @@ app.listen(Port, (err)=>{
         console.error(err);
     }   
     console.log(`Server is running on port ${Port}`);
+    connectDB();
 });
