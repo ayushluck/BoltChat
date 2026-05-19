825e0fa74a44c27b4943f2fcad74fca128b7efdd
ayushluck <luckyayush2005@gmail.com>
2026-05-19 12:00:53 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,13 +3,22 @@ const app = express();
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import path from 'path';
 
 dotenv.config();
+const __dirname = path.resolve();
 const Port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (_,res)=>{
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
 
 app.listen(Port, (err)=>{
     if(err){
