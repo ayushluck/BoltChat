75dce1c7e6454cdb5009f7d05793709fd0840f43
ayushluck <luckyayush2005@gmail.com>
2026-06-04 11:45:16 +0530

    Simplify Arcjet protection

--- Backend/src/middleware/arcjet.middleware.js
+++ Backend/src/middleware/arcjet.middleware.js
@@ -4,15 +4,7 @@ export const arcjetProtection = async (req, res, next) => {
     try {
         const decision = await aj.protect(req);
         if (decision.isDenied) {
-            if (decision.reason.isRateLimit()) {
-                return res.status(429).json({ message: "Rate limit exceeded. Please try again later." });
-            }
-            else if (decision.reason.isBot()) {
-                return res.status(403).json({ message: "Bot traffic is not allowed." });
-            }
-            else {
-                return res.status(403).json({ message: "Access denied by security policy" });
-            }
+            return res.status(403).json({ message: "Access denied by security policy" });
         }
         next();
     } catch (err) {
